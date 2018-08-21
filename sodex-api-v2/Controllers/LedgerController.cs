47f04f7f01f03c5b8f612791e4ee8cfac3c9bd04
NoahOliverRIgonan <oliverrigonan@gmail.com>
2018-08-21 16:25:44 +0800

    Ledger Controller

--- sodex-api-v2/Controllers/LedgerController.cs
+++ sodex-api-v2/Controllers/LedgerController.cs
@@ -7,44 +7,24 @@ using System.Web.Http;
 
 namespace sodex_api_v2.Controllers
 {
-    [RoutePrefix("api/Ledger")] 
+    [Authorize, RoutePrefix("api/ledger")]
     public class LedgerController : ApiController
     {
+        // ============
+        // Data Context
+        // ============
         private Data.SodexDatabaseDataContext db = new Data.SodexDatabaseDataContext();
 
-        // GET api/ledger/getBalance/cardNumber
-        [HttpGet, Route("GetBalance")]
-        public Decimal GetBalance(string cardNumber)
-        {
-            decimal balance = 0;
-
-            var card = from d in db.MstCards 
-                       where d.CardNumber == cardNumber 
-                       select d;
-
-            if (card.Any())
-            {
-                balance = card.FirstOrDefault().Balance;
-            }
-
-            return balance;
-        }
-
-        // GET api/ledger/getStatus
-        [HttpGet, Route("GetStatus")]
-        public String GetStatus()
-        {
-            return "OK";
-        }
-
-        // GET api/ledger/getLedger
-        [HttpGet, Route("GetLedger/{cardNumber}/{dateStart}/{dateEnd}")]
+        // =============
+        // List - Ledger
+        // =============
+        [HttpGet, Route("list/{cardNumber}/{dateStart}/{dateEnd}")]
         public List<Models.TrnLedger> GetLedger(string cardNumber, string dateStart, string dateEnd)
         {
             var TrnLedgerData = from d in db.TrnLedgers
-                                where d.MstCard.CardNumber == cardNumber &&
-                                      (d.LedgerDateTime >= Convert.ToDateTime(dateStart) &&
-                                       d.LedgerDateTime <= Convert.ToDateTime(dateEnd))
+                                where d.MstCard.CardNumber == cardNumber
+                                && d.LedgerDateTime >= Convert.ToDateTime(dateStart)
+                                && d.LedgerDateTime <= Convert.ToDateTime(dateEnd)
                                 orderby d.Id ascending
                                 select new Models.TrnLedger
                                 {
